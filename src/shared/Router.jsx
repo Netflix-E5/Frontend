277aaccb5a17e2a1a3f0814013bd60bfa22527a4
hj <dhzhd123@naver.com>
2022-12-27 23:32:20 +0900

    FEAT: add picked page view

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -5,15 +5,17 @@ import Header from "./Header";
 import Home from "../pages/Home";
 import Picked from "../pages/Picked";
 import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/signin" element={<Signin />} />
         <Route path="/" element={<Home />} />
-        <Route path="/user/picked" element={<Picked />} />
+        <Route path="/user/picked/:id" element={<Picked />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
