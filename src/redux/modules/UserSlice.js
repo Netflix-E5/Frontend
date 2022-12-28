dae651b00b4d03c46b8ac0bb202b1da220df8386
hj <dhzhd123@naver.com>
2022-12-28 15:16:21 +0900

    FEAT: add singup api

--- src/redux/modules/UserSlice.js
+++ src/redux/modules/UserSlice.js
@@ -1,9 +1,27 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apis } from "../../shared/api";
 
-const __signIn = createAsyncThunk("signIn", async (payload, thunkAPI) => {
-  console.log(payload);
-  thunkAPI.fulfillWithValue(payload);
-});
+export const __postSignIn = createAsyncThunk(
+  "postSignIn",
+  async (payload, thunkAPI) => {
+    thunkAPI.fulfillWithValue(payload);
+  }
+);
+
+export const __postSignup = createAsyncThunk(
+  "postSignup",
+  async (payload, thunkAPI) => {
+    try {
+      await apis.signup(payload).then((respose) => {
+        return thunkAPI.fulfillWithValue(respose.data);
+      });
+      window.alert("회원가입 성공!");
+    } catch (error) {
+      window.alert(error.response.data.errorMessage);
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 const initialState = {
   response: {},
@@ -15,9 +33,14 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   extraReducers: (builder) => {
-    builder.addCase(__signIn.fulfilled, (state, action) => {
-      console.log(action.payload);
-    });
+    builder
+      //
+      .addCase(__postSignIn.fulfilled, (state, action) => {
+        state.response = action.payload;
+      })
+      .addCase(__postSignup.fulfilled, (state, action) => {
+        state.response = action.payload;
+      });
   },
 });
 
